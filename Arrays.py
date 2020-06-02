617acee7f38b709208da4c4adb3b2e4c03849ac4
Shingie Maboreke <shingiemabz@gmail.com>
2020-06-02 20:56:28 +0200

    Update on the exercises practice

--- Arrays.py
+++ Arrays.py
@@ -124,3 +124,64 @@ def hourglassSum(arr):
             max_sum = max(sum,max_sum)
 
     return(max_sum)
+# //-------------------------------------------------QUESTION 5----------------------------------------------------------
+# // You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates.
+# // You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order.
+# //
+# // For example, given the array arr = [7, 1, 3, 2, 4, 5, 6] we perform the following steps:
+# //
+# // i   arr                         swap (indices)
+# // 0   [7, 1, 3, 2, 4, 5, 6]   swap (0,3)
+# // 1   [2, 1, 3, 7, 4, 5, 6]   swap (0,1)
+# // 2   [1, 2, 3, 7, 4, 5, 6]   swap (3,4)
+# // 3   [1, 2, 3, 4, 7, 5, 6]   swap (4,5)
+# // 4   [1, 2, 3, 4, 5, 7, 6]   swap (5,6)
+# // 5   [1, 2, 3, 4, 5, 6, 7]
+# //
+# // It took 5 swaps to sort the array.
+# //------------------------------------------------SOLUTION-------------------------------------------------------------
+def minimumSwaps(arr):
+    swaps = 0
+    n = len(arr)
+    for i in range(0,n):
+        while arr[i] != i+1:
+            temp = arr[i]
+            arr[i] = arr[temp-1]
+            arr[temp-1] = temp
+            swaps+=1
+
+    return swaps
+
+# ---------------------------------------------------QUESTION 6------------------------------------------------------------
+# Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+#
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+#
+# The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+#
+# Example 1:
+#
+# Given nums = [3,2,2,3], val = 3,
+#
+# Your function should return length = 2, with the first two elements of nums being 2.
+#
+# It doesn't matter what you leave beyond the returned length.
+
+#---------------------------------------------------SOLUTION----------------------------------------------------------------
+ def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        n = len(nums)
+        while i < n:
+            if nums[i] == val:
+                nums[i] = nums[n - 1]
+                # reduce array size by one
+                n-=1
+            else:
+                i+=1
+
+        return n
